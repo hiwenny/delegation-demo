6f2abdb35c079d77e6a6d8841471c3fce9dc992c
Wenny Hidayat <wenny.h@live.com>
2020-10-13 07:36:30 +1100

    Added more demos

--- src/App.js
+++ src/App.js
@@ -5,14 +5,110 @@ import ComponentWithListener from './ComponentWithListener';
 function App() {
   return (
     <>
-      Sample component with default renderProps
       <ComponentWithListener />
-      <ComponentWithListener optionalComponent={(onClickHandler) => <div onClick={onClickHandler}>Custom Button</div>} />
-      <ComponentWithListener optionalComponent={(onClickHandler) => (
-        <div onClick={onClickHandler}>
-          <div onClick={() => alert('nested')}>Custom Button with Nested onClick</div>
-        </div>
-      )} />
+          <ComponentWithListener
+            optionalComponent={(onClickHandler) => (
+              <div
+                onClick={onClickHandler}
+                style={{
+                  color: 'white',
+                  background: '#c3d9c9',
+                  padding: '5px 10px',
+                  cursor: 'pointer',
+                  borderRadius: '20px',
+                }}
+              >
+                Custom Button, handler from ComponentWithListener
+              </div>
+            )}
+          />
+          <ComponentWithListener
+            optionalComponent={(onClickHandler) => (
+              <div
+                onClick={onClickHandler}
+                style={{
+                  color: 'white',
+                  background: '#c3d9c9',
+                  padding: '5px 10px',
+                  cursor: 'pointer',
+                  borderRadius: '20px',
+                }}
+              >
+                Custom Button, handler from ComponentWithListener
+                <div
+                  onClick={() => alert('Nested inside renderProps component with own onClick')}
+                  style={{
+                    color: '#84a38c',
+                    background: '#e6e6e6',
+                    padding: '5px 10px',
+                    cursor: 'pointer',
+                    borderRadius: '20px',
+                  }}
+                >
+                  Nested inside renderProps component with own onClick
+                </div>
+              </div>
+            )}
+          />
+          <ComponentWithListener
+            optionalComponent={(onClickHandler) => (
+              <div
+                onClick={onClickHandler}
+                style={{
+                  color: 'white',
+                  background: '#c3d9c9',
+                  padding: '5px 10px',
+                  cursor: 'pointer',
+                  borderRadius: '20px',
+                }}
+              >
+                Custom Button, handler from ComponentWithListener
+                <div
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    alert('Nested inside renderProps component with own onClick');
+                  }}
+                  style={{
+                    color: '#84a38c',
+                    background: '#e6e6e6',
+                    padding: '5px 10px',
+                    cursor: 'pointer',
+                    borderRadius: '20px',
+                  }}
+                >
+                  Nested inside renderProps component with own onClick and stopPropagation
+                </div>
+              </div>
+            )}
+          />
+          <div
+            role="placeholder"
+            onClick={() => alert('Default Component')}
+            style={{
+              color: 'white',
+              background: '#84a38c',
+              padding: '5px 10px',
+              cursor: 'pointer',
+              borderRadius: '20px',
+            }}
+          >
+            Default Button (on parent)
+            <div
+              onClick={(e) => {
+                e.stopPropagation();
+                alert('nested on parent');
+              }}
+              style={{
+                color: '#84a38c',
+                background: '#e6e6e6',
+                padding: '5px 10px',
+                cursor: 'pointer',
+                borderRadius: '20px',
+              }}
+            >
+              with Nested onClick (not a renderProp)
+            </div>
+          </div>
       {/* TODO test with other listeners */}
       {/* <ComponentWithListener optionalComponent={(onClickHandler) => (
         <div onClick={onClickHandler}>
--- src/ComponentWithListener.js
+++ src/ComponentWithListener.js
@@ -1,34 +1,30 @@
+/* eslint-disable */
 import React, { useCallback } from 'react';
 
-const ComponentWithListener = ({ optionalComponent }) => {
-  const onClickHandler = useCallback(
-    event => {
-      console.log(event.currentTarget);
-    },
-  );
+const ComponentWithListener = ({ optionalComponent }: { optionalComponent?: any }) => {
+  const onClickHandler = useCallback((event) => {
+    alert('Handled from ComponentWithListener');
+  }, []);
 
   let content;
 
-  if (typeof optionalComponent === 'function') {
-    // this renderprops has a bug where the nested onCLick event doesn't get actioned
-    // show with colour change
-    // try with other events as well
+  if (optionalComponent && typeof optionalComponent === 'function') {
     // try in safari
 
-    content = optionalComponent({
-      onClickHandler,
-    });
+    content = optionalComponent(onClickHandler);
   } else {
     content = (
       <div
+        role="placeholder"
         onClick={onClickHandler}
-        style={{color: 'blue'}}
+        style={{ color: 'white', background: '#84a38c', padding: '5px 10px', cursor: 'pointer', borderRadius: '20px' }}
       >
         Default Button
       </div>
     );
   }
-return <div>{content}</div>
-}
+  return <>{content}</>;
+};
 
 export default ComponentWithListener;
+/* eslint-enable */
